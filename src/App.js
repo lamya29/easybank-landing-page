8048d829dbf04a45e57d3c36066aa40ac2b06394
lamya29 <lamyaelb2019d@gmail.com>
2022-07-07 18:06:47 +0100

    create pricing component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Navbar from './Components/Navbar/Navbar'
+import Home from './Components/Home/Home';
+import About from './Components/About/About';
+import Blog from './Components/Blog/Blog';
+import Footer from './Components/Footer/Footer'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar  />
+      <Home />
+      <About  />
+      <Blog />
+      <Footer />
     </div>
-  );
+  )
 }
 
 export default App;
+ 
